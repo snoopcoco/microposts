3925b46c2d5a469ba2bb1df622c0a74248020e22
snoopcoco <snoop5572@gmail.com>
2015-10-12 12:32:56 +0000

    add initial setups

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module Microposts
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      #テストコードとJavaScript、CSS(SCSS)、ヘルパのファイルを自動生成しないため
+    end
   end
 end
