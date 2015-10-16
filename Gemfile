143eb27349bd90db261b620bc3897b485bae3cca
snoopcoco <snoop5572@gmail.com>
2015-10-16 12:21:27 +0000

    add sign up and user detail

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass' #bootstrapを追加
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7' #パスワード付きのモデルを仕様するため
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: {maximum: 50 }
+  VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255 }, format: { with: VALID_EMAIL_REGX }, uniqueness: {case_sensitice: false }
+  
+  has_secure_password
 end
