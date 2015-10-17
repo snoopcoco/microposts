5ae7dd5e165fe7609e96bb2efdf357114659e7d6
snoopcoco <snoop5572@gmail.com>
2015-10-17 02:36:53 +0000

    add update and place

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to @user, notice: 'Edit your profile' #flashとnoticeの違い
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+    
   def show 
    @user = User.find(params[:id])
   end
@@ -21,6 +35,10 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :place)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
