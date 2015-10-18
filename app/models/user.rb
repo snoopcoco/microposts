4eae635b51e2df31f45082490ea80281832320af
snoopcoco <snoop5572@gmail.com>
2015-10-18 12:03:21 +0000

    Oct 18th

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50 }
   VALID_EMAIL_REGX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255 }, format: { with: VALID_EMAIL_REGX }, uniqueness: {case_sensitice: false }
-  validates :place, presence: true
+  validates :place, presence: true, on: :update
   
   has_secure_password
 end
