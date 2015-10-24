7db0a504e64344363d5af504471303fdaeb136d9
snoopcoco <snoop5572@gmail.com>
2015-10-24 13:25:17 +0000

    add home view's timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,8 @@ class User < ActiveRecord::Base
   def following?(other_user) #フォローしているかどうか
     following_users.include?(other_user)
   end
+  
+  def feed_items
+    Micropost.where(user_id: following_user_ids + [self.id])
+  end
 end
